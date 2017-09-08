0e3cdb5979f3815eb092e53c06d5fb1fbce9a508
Corban Brook <corbanbrook@gmail.com>
2017-09-08 12:17:11 -0400

    Adding query params to Route

--- packages/mobx-little-router/src/model/createRoute.js
+++ packages/mobx-little-router/src/model/createRoute.js
@@ -1,12 +1,14 @@
 // @flow
-import type { Params, Route, RouteStateTreeNode } from './types'
+import createRouteKey from './createRouteKey'
+import type { Params, Query, Route, RouteStateTreeNode } from './types'
 
-export default function createRoute<C: Object,D: Object>(node: RouteStateTreeNode<C,D>, params: Params, segment: string): Route<C,D> {
+export default function createRoute<C: Object, D: Object>(node: RouteStateTreeNode<C, D>, segment: string, params: Params, query: Query): Route<C,D> {
   return {
     node: node,
-    key: `${node.value.key}${segment.toString()}`,
+    key: createRouteKey(node, segment, query),
     segment,
     params,
+    query,
     context: node.value.getContext(),
     data: { ...node.value.getData(), transitionState: null },
     onTransition: node.value.onTransition
